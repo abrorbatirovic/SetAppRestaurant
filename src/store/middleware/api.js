a6abd69bf97feed5dc49a8da37151bbc21e418d3
abrorbatirovic <ashurmatovabror1107@gmail.com>
2021-11-06 23:35:19 +0500

    started

---
+++ src/store/middleware/api.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const api = ({dispatch})=>(next)=>(action)=>{
+
+    if(action.type !== 'api/apiCall'){
+        next(action)
+        return
+    }
+
+    next(action)
+
+    const {url, method, data, onSuccess, onFile} = action.payload
+
+    axios({
+        baseURL: '',
+        url,
+        method,
+        data
+    }).then((res)=>{
+        dispatch({
+            type: onSuccess,
+            payload: res.data
+        })
+    }).catch((err)=>{
+        dispatch({
+            type: onFile,
+            payload: err.data
+        })
+    })
+
+}
+
+export default api
